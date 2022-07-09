696eca7ef086527602b67a942c82ea590d5bf68f
Yoon Sewook <108972960+LimiteDiTempo@users.noreply.github.com>
2022-07-09 17:09:23 +0900

    Add files via upload

--- script.py
+++ script.py
@@ -6,8 +6,7 @@ def write(name, desc):
 
 # 하단 버튼 링크 연결 함수
 def button(*args):
-  link = "https://www.naver.com/" # https:// 꼭 붙여야 연결됩니다!
-  webbrowser.open(link)
+  link = "https://www.google.com/" # https://www.instagram.com/sew00k/webbrowser.open(link)
 
 # 배경 색깔 설정 함수
 def background(color):
@@ -21,19 +20,19 @@ def information(info):
     write("b" + str(i), info[key[i]])
 
 # 배경 색깔 설정
-colors = ["green", "blue"]
+colors = ["white", "black"]
 background(colors)
 
 # 이름과 설명, 버튼에 들어갈 글 설정
-write("name", "이름")
-write("description", "설명")
-write("button", "버튼")
+write("name", "윤세욱")
+write("description", "")
+write("button", "Instagram")
 
 # 상세설명에 들어갈 제목과 글 설정
 informations = {
-  "제목1": "내용1",
-  "제목2": "내용2",
-  "제목3": "내용3",
-  "제목4": "내용4"
+  "나이": "16살",
+  "학교": "왕선중학교",
+  "좋아하는 것": "게임(롤)",
+  "MBTI": "INTP"
 }
 information(informations)
